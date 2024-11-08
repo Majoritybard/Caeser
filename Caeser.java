7d1f20c252d85a5595035dfc7ea0b3aa2141f239
MajorityBard <sven@hartmann.vc>
2024-11-08 10:39:37 +0100

    Erste Änderungen

---
+++ Caeser.java
@@ -0,0 +1,52 @@
+public class Caeser {
+   private String Kt;
+    String gt="";
+    int S;
+    public Caeser(){
+        Kt="";
+
+
+
+    }
+    public void verschlüsseln(){
+
+        for (int i=0;i<Kt.charat(i)){
+            gt.getchar(i)=(char)(Kt.char(i)+S)
+
+        }
+
+
+    }
+
+    private void entschlüsseln(){
+
+
+    }
+    private int getASCII(char pWert){
+
+
+    }
+
+    private char getChar(int pWert){
+    return(char)pWert;
+
+    }
+
+    public void setKt(String pWert){
+        this.Kt=pWert;
+
+
+    }
+
+    private String getKt(){
+
+
+    }
+
+
+    private void setS(int pWert){
+
+
+    }
+
+}
